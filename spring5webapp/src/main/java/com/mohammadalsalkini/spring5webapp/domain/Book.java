9d0701c45e365f83ba999b7b12c0f4df1defc6ea
mohammadsalkini <eng.salkini@hotmail.com>
2020-03-31 11:10:20 +0200

    add Book class

--- spring5webapp/src/main/java/com/mohammadalsalkini/spring5webapp/domain/Book.java
+++ spring5webapp/src/main/java/com/mohammadalsalkini/spring5webapp/domain/Book.java
@@ -1,9 +1,48 @@
 package com.mohammadalsalkini.spring5webapp.domain;
 
+import java.util.Set;
+
 /**
  * @author engsa
  * @project spring5webapp
  * @created 31.03.2020 - 11:07
  */
 public class Book {
+
+    private String title;
+    private String isbn;
+    private Set<Author> authors;
+
+    public Book() {
+    }
+
+    public Book(String title, String isbn, Set<Author> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.authors = authors;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
 }
