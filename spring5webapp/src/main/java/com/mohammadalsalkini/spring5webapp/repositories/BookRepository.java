61dd6b2a41e893183f689eec1c21747b9f0d46e6
mohammadsalkini <eng.salkini@hotmail.com>
2020-03-31 11:33:03 +0200

    add repositories

---
+++ spring5webapp/src/main/java/com/mohammadalsalkini/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package com.mohammadalsalkini.spring5webapp.repositories;
+
+/**
+ * @author engsa
+ * @project spring5webapp
+ * @created 31.03.2020 - 11:30
+ */
+public interface BookRepositories {
+}
