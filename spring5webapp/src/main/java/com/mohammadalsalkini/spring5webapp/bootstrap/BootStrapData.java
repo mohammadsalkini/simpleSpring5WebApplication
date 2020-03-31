1c973e08acb52b3970ca5dce2f50a4b0ee54b750
mohammadsalkini <eng.salkini@hotmail.com>
2020-03-31 12:03:34 +0200

    add new data

--- spring5webapp/src/main/java/com/mohammadalsalkini/spring5webapp/bootstrap/BootStrapData.java
+++ spring5webapp/src/main/java/com/mohammadalsalkini/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package com.mohammadalsalkini.spring5webapp.bootstrap;
 
 import com.mohammadalsalkini.spring5webapp.domain.Author;
 import com.mohammadalsalkini.spring5webapp.domain.Book;
+import com.mohammadalsalkini.spring5webapp.domain.Publisher;
 import com.mohammadalsalkini.spring5webapp.repositories.AuthorRepository;
 import com.mohammadalsalkini.spring5webapp.repositories.BookRepository;
+import com.mohammadalsalkini.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -18,15 +20,22 @@ public class BootStrapData implements CommandLineRunner {
 
     private  final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        Publisher publisher = new Publisher("Publisher first name", "Publisher last name");
+        publisher.setState("Vienna");
+        publisherRepository.save(publisher);
 
         Author mohammad = new Author("Mohammad", "Alsalkini");
         Book newBook = new Book("My new Book", "123321");
