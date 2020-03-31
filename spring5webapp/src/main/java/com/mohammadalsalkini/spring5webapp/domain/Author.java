9622ea5f13e0d667c464eee8480519aa92aed651
mohammadsalkini <eng.salkini@hotmail.com>
2020-03-31 11:25:51 +0200

    add JPA Entity

--- spring5webapp/src/main/java/com/mohammadalsalkini/spring5webapp/domain/Author.java
+++ spring5webapp/src/main/java/com/mohammadalsalkini/spring5webapp/domain/Author.java
@@ -1,5 +1,7 @@
 package com.mohammadalsalkini.spring5webapp.domain;
 
+import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -7,10 +9,18 @@ import java.util.Set;
  * @project spring5webapp
  * @created 31.03.2020 - 11:05
  */
+
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String firstName;
     private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
     public Author() {
@@ -20,6 +30,14 @@ public class Author {
         this.firstName = firstName;
         this.lastName = lastName;
         this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
@@ -44,5 +62,28 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
     }
 }
--- spring5webapp/src/main/java/com/mohammadalsalkini/spring5webapp/domain/Book.java
+++ spring5webapp/src/main/java/com/mohammadalsalkini/spring5webapp/domain/Book.java
@@ -1,5 +1,7 @@
 package com.mohammadalsalkini.spring5webapp.domain;
 
+import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -7,10 +9,20 @@ import java.util.Set;
  * @project spring5webapp
  * @created 31.03.2020 - 11:07
  */
+
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String title;
     private String isbn;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors;
 
     public Book() {
@@ -20,6 +32,14 @@ public class Book {
         this.title = title;
         this.isbn = isbn;
         this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -44,5 +64,28 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
     }
 }
