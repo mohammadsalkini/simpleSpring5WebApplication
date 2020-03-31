1296908c1bc7021d65620adcb19c309be359b6c8
mohammadsalkini <eng.salkini@hotmail.com>
2020-03-31 11:45:29 +0200

    add repositories

--- spring5webapp/src/main/java/com/mohammadalsalkini/spring5webapp/repositories/AuthorRepository.java
+++ spring5webapp/src/main/java/com/mohammadalsalkini/spring5webapp/repositories/AuthorRepository.java
@@ -1,9 +1,12 @@
 package com.mohammadalsalkini.spring5webapp.repositories;
 
+import com.mohammadalsalkini.spring5webapp.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+
 /**
- * @author engsa
+ * @author Mohammad Alsalkini
  * @project spring5webapp
  * @created 31.03.2020 - 11:29
  */
-public interface AuthorRepository {
+public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
--- spring5webapp/src/main/java/com/mohammadalsalkini/spring5webapp/repositories/BookRepository.java
+++ spring5webapp/src/main/java/com/mohammadalsalkini/spring5webapp/repositories/BookRepository.java
@@ -1,9 +1,12 @@
 package com.mohammadalsalkini.spring5webapp.repositories;
 
+import com.mohammadalsalkini.spring5webapp.domain.Book;
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * @author engsa
  * @project spring5webapp
  * @created 31.03.2020 - 11:30
  */
-public interface BookRepositories {
+public interface BookRepository extends CrudRepository<Book, Long> {
 }
