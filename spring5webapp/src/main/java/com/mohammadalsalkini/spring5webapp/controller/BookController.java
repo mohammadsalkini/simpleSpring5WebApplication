c72da4e427e23e88dea4d6910194bf484867f412
mohammadsalkini <eng.salkini@hotmail.com>
2020-03-31 13:10:33 +0200

    add Book controller

---
+++ spring5webapp/src/main/java/com/mohammadalsalkini/spring5webapp/controller/BookController.java
@@ -0,0 +1,32 @@
+package com.mohammadalsalkini.spring5webapp.controller;
+
+import com.mohammadalsalkini.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Mohammad Alsalkini
+ * @project spring5webapp
+ * @created 31.03.2020 - 13:06
+ */
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+    }
+
+}
